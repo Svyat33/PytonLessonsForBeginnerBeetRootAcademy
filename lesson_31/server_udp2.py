d40dfa15be34ff0ffcf0d2c9a327b7240404d433
Святослав <svyatka@gmail.com>
2020-05-18 20:28:05 +0300

    вариант немного разбить на детали

--- lesson_31/server_udp2.py
+++ lesson_31/server_udp2.py
@@ -1,47 +1,51 @@
 import socket
 import argparse
+import traceback
+
+from command.stat import cmd_stat
+
+msgFromServer = "Hi UDP Client"
+bytesToSend = str.encode(msgFromServer)
+
+
+def cmd_cmd(message, address, UDPServerSocket):
+    pass
+
+def load_conf():  # Function for load server configuration
+    parser = argparse.ArgumentParser(description="Server UDP")
+    parser.add_argument('--host', '-i', default="127.0.0.1", required=False, help='Which IP adress')
+    parser.add_argument('--port', '-p', default=5000, type=int, required=False, help='Which port')
+    parser.add_argument('--buffer_size', '-b', default=1024, type=int, required=False, help='Buffer size')
+    args = parser.parse_args()
+    print(args)
+    return args
+def route(message, address, UDPServerSocket):
+    print("Message from Client:{}".format(message))
+    print("Client IP Address:{}".format(address))
+    # Sending a reply to client
+    if cmd_stat(message, address, UDPServerSocket):
+        return
+    elif cmd_cmd(message, address, UDPServerSocket):
+        return
+
+    UDPServerSocket.sendto(bytesToSend, address)
+
+def start_server(cfg: dict):  # Function for start server
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # Bind to address and ip
+    UDPServerSocket.bind((cfg['host'], cfg['port']))
+    while True:
+        message, address = UDPServerSocket.recvfrom(cfg['buffer_size'])  # read data from socket
+        route(message, address, UDPServerSocket)
+
 
 if __name__ == '__main__':
 
-    localIP = "127.0.0.1"
-    localPort = 5000
-    bufferSize = 1024
-    global conf, bytesToSend
-    conf = [localIP, localPort, bufferSize]
-    msgFromServer = "Hi UDP Client"
-    bytesToSend = str.encode(msgFromServer)
-
-
-    def load_conf():    # Function for load server configuration
-        parser = argparse.ArgumentParser(description="Server UDP")
-        parser.add_argument('--host', '-i', default=conf[0], required=False, help='Which IP adress')
-        parser.add_argument('--port', '-p', default=conf[1], type=int, required=False, help='Which port')
-        parser.add_argument('--buffer_size', '-b', default=conf[2], type=int, required=False, help='Buffer size')
-        args = parser.parse_args()
-        print(args)
-        global UDPServerSocket
-        UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-        # Bind to address and ip
-        UDPServerSocket.bind((localIP, localPort))
-
-    def start_server(): # Function for start server
-        data = UDPServerSocket.recvfrom(bufferSize)  # read data from socket
-        message = data[0]
-        address = data[1]
-        clientMsg = "Message from Client:{}".format(message)
-        clientIP = "Client IP Address:{}".format(address)
-        print(clientMsg)
-        print(clientIP)
-
-        # Sending a reply to client
-        UDPServerSocket.sendto(bytesToSend, address)
-
-    load_conf()
+    config = load_conf()
     print("UDP server up and listening")
-    try:
-        # Listen for incoming datagrams
-        while (True):
-            start_server()
-    except:
-        raise ConnectionError
+
+    while True:
+        try:
+            start_server(config)
+        except:
+            print(traceback.format_exc())
