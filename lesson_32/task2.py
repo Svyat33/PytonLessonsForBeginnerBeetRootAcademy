bdbf87aa46ed97faaddf60ccbf8fe033f1848b0d
Святослав <svyatka@gmail.com>
2020-05-29 12:58:11 +0300

    правки
    подключаемся внутри.
    в цикле собираем обьект и потом один раз его пишем. Тогда файл можно будет открыть и json модулем прочесть обратно. А если писать по частям то назад прочесть будет сложно.

--- lesson_32/task2.py
+++ lesson_32/task2.py
@@ -2,17 +2,17 @@ import json
 import requests
 import urllib.request
 
-url = urllib.request.urlopen('https://api.pushshift.io/reddit/comment/search/')
-obj = json.load(url)
 
 if __name__ == "__main__":
-
+    url = urllib.request.urlopen('https://api.pushshift.io/reddit/comment/search/')
+    obj = json.load(url)
     with open('data.json', 'w') as outfile:
         i = 0
+        store = []
         while i < 10:
             obj1 = obj['data'][i]['author']
             obj2 = obj['data'][i]['body']
+            store.append({'author': obj1, 'comments': obj2})
             print("Author: {}\nComments: {}\n{:*^50}".format(obj1, obj2, 'next comment'))
             i += 1
-            json.dump(obj1, outfile)
-            json.dump(obj2, outfile)
+        json.dump(store, outfile)
